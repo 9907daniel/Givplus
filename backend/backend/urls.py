2eeae16abf8b35665c9887a044a0b2f07988457b
9907daniel <daniel.9907@hotmail.com>
2023-02-26 23:12:05 +0900

    frontend & backend for authentication + base structure

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path, include
-from base import views
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('base.urls')),
-    # Send to base.urls.py
-    # Send rest of endpoints behind / to base.urls
-
+    path('api/user/', include('account.urls')),
+    path('api/', include('api.urls')),
 ]
