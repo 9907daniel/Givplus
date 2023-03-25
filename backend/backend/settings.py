a3e16ec60f3534290994a9e1474213157ec308f6
9907daniel <daniel.9907@hotmail.com>
2023-03-25 08:39:45 +0000

    changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -16,7 +16,7 @@ import os
 import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 dotenv.read_dotenv()
 
@@ -141,8 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/home/daniel/Givplus/backend/static/'
+STATIC_URL = '/backend/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 MEDIA_URL = '/media/'
 
