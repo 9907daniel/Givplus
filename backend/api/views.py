bfea1c29762f43370f85b592098986cfa9e23b1c
9907daniel <daniel.9907@hotmail.com>
2023-03-28 11:33:26 +0000

    changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -152,7 +152,7 @@ def country_list(request, country_id=None):
                                 description_dict[field_name] = row[key]
 
                         mydata = Country(
-                            number=row['Number'],
+                            #number=row['Number'],
                             name=row['Official Name'], 
                             continent=row['Continent'],
                             gdp=row['GDP'],
@@ -164,8 +164,9 @@ def country_list(request, country_id=None):
                             description=description_dict, 
                             # platforms=row['Platforms'],
                             location_lat=row['Latitude'], 
-                            location_lng=row['Longitude']
-                        )
+                            location_lng=row['Longitude'],
+                            Number = row['Number']
+			)
                         mydata.save()
 
         # Return the new data as a response
